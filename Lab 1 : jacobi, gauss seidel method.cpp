c8493f17afb9c5e683d5280269e8cc31bba04e1b
rahman-arifur <113102124+rahman-arifur@users.noreply.github.com>
2024-09-09 17:34:17 +0600

    Create Lab 1 : jacobi, gauss seidel method.cpp

---
+++ Lab 1 : jacobi, gauss seidel method.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+vector<double> coeff[500];
+double C[500];
+const double eps = 1e-6;
+
+int N = 3;
+bool input = false; // make it true for taking custom input for the variable size
+
+double getvalue(int eqnNo, vector<double>& var) { // equation no, current variables value
+    // x = (k - (by + cz)) / N
+    double ret = C[eqnNo];
+    for (int i = 0; i < N; i++) {
+        if(i != eqnNo)
+            ret -= (coeff[eqnNo][i] * var[i]);
+    }
+    return ret / coeff[eqnNo][eqnNo];
+}
+
+void jacobi() {
+    vector<double> prev(N, 0.0), cur(N);
+
+    // 1st iteration
+    for (int i = 0; i < N; i++) {
+        cur[i] = getvalue(i, prev);
+        cout << cur[i] << " \n"[i == N - 1];
+    }
+    int iterations = 10;
+    vector<double> d(N, 0.0); // like dx, dy, dz
+    double eavg;
+    while (--iterations) {
+        eavg = 0.0;
+        swap(prev, cur);
+        for (int i = 0; i < N; i++) {
+            cur[i] = getvalue(i, prev);
+            d[i] = fabs(cur[i] - prev[i]);
+            cout << cur[i] << " \n"[i == N - 1];
+            eavg += d[i];
+        }
+        eavg = eavg / N;
+        if(eavg <= eps) break;
+    }
+}
+
+void gauss_seidel() {
+    vector<double> cur(N, 0), prev(N, 0);
+    // 1st iteration
+    for (int i = 0; i < N; i++) {
+        cur[i] = getvalue(i, cur);
+        cout << cur[i] << " \n"[i == N - 1];
+    }
+
+    int iterations = 10;
+
+    double eavg = 0;
+    vector<double> d(N);
+    while (--iterations) {
+        eavg = 0;
+        prev = cur;
+        for (int i = 0; i < N; i++) {
+            cur[i] = getvalue(i, cur);
+            cout << cur[i] << " \n"[i == N - 1];
+            d[i] = fabs(cur[i] - prev[i]);
+        }
+        eavg /= N;
+        // cout << eavg << " ";
+    }
+}
+
+int main() {
+    if (input) {
+        cout << "Number of variables: ";
+        cin >> N;
+    }
+
+    // cout << "Next N lines give input in the form\n";
+    // cout << "N + 1 inputs for the next N lines\n";
+    // cout << "a1 a2 ... aN = c\n";
+    for (int i = 0; i < N; i++) {
+        double x;
+        for (int j = 0; j < N; j++) {
+            cin >> x;
+            coeff[i].push_back(x);
+        }
+        cin >> C[i];
+    }
+
+    // jacobi();
+    // gauss_seidel();
+
+    return 0;
+}
